f41de46affc4b1898f3a172f71b96e64cc77dba6
Martin Zikmund <martin@zikmund.dev>
2025-04-29 15:17:26 +0200

    refactor: Raise AppInstance Activated only if the app is not launched yet

--- src/Uno.UI/UI/Xaml/Application.cs
+++ src/Uno.UI/UI/Xaml/Application.cs
@@ -109,7 +109,7 @@ namespace Microsoft.UI.Xaml
 			if (appInstance.GetActivatedEventArgs() is null)
 			{
 				// Default to launch activation args
-				appInstance.SetActivatedEventArgs(
+				appInstance.SetOrRaiseActivation(
 					AppActivationArguments.CreateLaunch(
 						new global::Windows.ApplicationModel.Activation.LaunchActivatedEventArgs(ActivationKind.Launch, GetCommandLineArgsWithoutExecutable())));
 			}
@@ -308,6 +308,7 @@ namespace Microsoft.UI.Xaml
 
 		internal void InvokeOnActivated(IActivatedEventArgs args)
 		{
+			Microsoft.Windows.AppLifecycle.AppInstance.GetCurrent().SetOrRaiseActivation(AppActivationArguments.FromActivatedEventArgs(args));
 			OnActivated(args);
 			WasLaunched = true;
 		}
